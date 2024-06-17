67722b5786e641e07022bf77ef26a8863366ace1
Tony Spiro <tspiro@tonyspiro.com>
2024-06-17 14:43:45 -0700

    add: fileRejections message

--- cosmic/blocks/file-upload/FileUpload.tsx
+++ cosmic/blocks/file-upload/FileUpload.tsx
@@ -39,11 +39,11 @@ export function FileUpload({
     },
     [filesInQueue]
   );
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({
-    onDrop,
-    maxSize: maxSize,
-  });
-
+  const { getRootProps, getInputProps, isDragActive, fileRejections } =
+    useDropzone({
+      onDrop,
+      maxSize: maxSize,
+    });
   const files = filesInQueue.map((file: File) => (
     <li key={file.name} className="mb-4">
       {file.type.indexOf("image") !== -1 ? (
@@ -128,6 +128,22 @@ export function FileUpload({
         <div className="border flex border-red-600 dark:border-red-400 dark:text-red-400 p-4 text-red-600 rounded-xl">
           <XCircle className="relative top-1 mr-4 h-4 w-4 text-red-500" />{" "}
           Upload error!
+        </div>
+      ) : (
+        ""
+      )}
+      {fileRejections.length ? (
+        <div className="border mt-4 flex border-red-600 dark:border-red-400 dark:text-red-400 p-4 text-red-600 rounded-xl">
+          <XCircle className="relative top-1 mr-4 h-4 w-4 text-red-500" />{" "}
+          <div>
+            {fileRejections.map(({ file, errors }) => {
+              return (
+                <div key={file.name}>
+                  Error uploading {file.name}. Message: {errors[0].message}
+                </div>
+              );
+            })}
+          </div>
         </div>
       ) : (
         ""
